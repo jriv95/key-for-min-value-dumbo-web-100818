9431cdd66d32683f0aaa7f88b00b5a058bc7fa2a
Joel Rivera <joel.rivera7995@gmail.com>
2018-10-04 10:21:08 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,5 +6,11 @@ def key_for_min_value(name_hash)
   min_key = nil
   name_hash.each do |name, value|
     if min_value == nil
+      min_value = value
+      min_key = name
+    elsif min_value > value
+      min_value = value
+      min_key = name
   end
+end
 end
